e360988d8fe7e6f3c1b86bb71b7a76c776bbd8b8
Heidi <zhanghaiyao@outlook.com>
2024-10-10 14:33:15 +1300

    birthday card

---
+++ app.js
@@ -0,0 +1,12 @@
+// select modal-btn,modal-overlay,close-btn
+// listen for click events on modal-btn and close-btn
+// when user clicks modal-btn add .open-modal to modal-overlay
+// when user clicks close-btn remove .open-modal from modal-overlay
+
+const modalBtn = document.querySelector('.modal-btn');
+
+
+
+modalBtn.addEventListener("click",function(){
+    location.href = 'https://www.greetingsisland.com/ecard/pocrpfltyfw4';
+});

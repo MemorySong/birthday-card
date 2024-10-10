164c8423ad710b6e6aab39002e1e29c07fb54681
Heidi <zhanghaiyao@outlook.com>
2024-10-10 22:22:44 +1300

    finish

--- app.js
+++ app.js
@@ -3,10 +3,19 @@
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
 
+//const modalBtn = document.querySelector('.modal-btn');
+
+
+
+
 const modalBtn = document.querySelector('.modal-btn');
-
-
+const modal = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
 
 modalBtn.addEventListener("click",function(){
-    location.href = 'https://www.greetingsisland.com/ecard/pocrpfltyfw4';
+    modal.classList.add("open-modal");
+});
+
+closeBtn.addEventListener("click",function(){
+    modal.classList.remove("open-modal");
 });
